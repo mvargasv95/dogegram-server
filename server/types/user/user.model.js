d834937466c9178b6f53fd83a0e43ac025dfa2f9
mvargasv95 <minorvargas46@gmail.com>
2020-01-20 21:46:11 -0600

    user mongoose model modified :rocket:

--- server/types/user/user.model.js
+++ server/types/user/user.model.js
@@ -1,10 +1,63 @@
 import mongoose from 'mongoose'
+import bcrypt from 'bcrypt'
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true
-  }
+    required: true,
+    minlength: 2,
+    maxlength: 20
+  },
+  username: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50,
+    unique: true,
+    index: true,
+    trim: true,
+    lowercase: true
+  },
+  email: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255,
+    unique: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+    maxlength: 1024
+  },
+  avatar: String,
+  posts: [],
+  followers: [],
+  following: []
 })
+
+userSchema.pre('save', function(next) {
+  if (!this.isModified('password')) return next()
+
+  bcrypt.hash(this.password, 8, (err, hash) => {
+    if (err) return next(err)
+    this.password = hash
+    next()
+  })
+})
+
+userSchema.methods.checkPassword = function(password) {
+  const passwordHash = this.password
+
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(password, passwordHash, (err, same) => {
+      if (err) return reject(err)
+
+      resolve(same)
+    })
+  })
+}
 
 export const User = mongoose.model('user', userSchema)
